71b9fe124a36f92e8a7e16663da92184b54ab334
Zackarias Montell <zackarias@montell.se>
2021-07-05 22:07:41 +0200

    Formatting

--- src/main/.eslintrc.js
+++ src/main/.eslintrc.js
@@ -1,40 +1,22 @@
 /* eslint-disable no-undef */
 module.exports = {
-    "env": {
-        "browser": false,
-        "node": true,
-        "es2021": true
+    env: {
+        browser: false,
+        node: true,
+        es2021: true,
     },
-    "extends": [
-        "eslint:recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": false
+    extends: ["eslint:recommended"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: false,
         },
-        "ecmaVersion": 12,
-        "sourceType": "common"
+        ecmaVersion: 12,
+        sourceType: "common",
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    rules: {
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "double"],
+        semi: ["error", "always"],
+    },
 };
