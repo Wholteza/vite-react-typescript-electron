44186c7bf8ef7f1ebb474ec656f44f8e5a2e4772
Zackarias Montell <zackarias@montell.se>
2021-07-05 21:16:24 +0200

    Initial setup

---
+++ src/renderer/app.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+const App = (): JSX.Element => (
+    <div>
+        <p>App is working!</p>
+    </div>
+);
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  root: "./src/renderer",
+  plugins: [reactRefresh()],
+  base: "./"
+})
